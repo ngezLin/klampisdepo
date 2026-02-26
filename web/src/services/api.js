e7f0ff2ca95855e80f4cf860b5f841a09b85f730
ngezlin <vincentiusf17@gmail.com>
2026-02-26 23:45:17 +0700

    feat: change api

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL || "https://api.klampisdepo.com",
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_URL || "https://api.klampisdepo.com",
+  // baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",
 });
 
 // Add request interceptor to attach token
