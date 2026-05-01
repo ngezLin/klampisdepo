fae46f623845a030b6d22cf83fdd0be88c0cb29b
Vincentius Franklin <vincentiusf17@gmail.com>
2026-05-02 00:28:08 +0700

    refactor: mobile repo

--- mobile/src/services/auth.service.ts
+++
@@ -1,8 +0,0 @@
-import api from "./api.service";
-
-export const login = (username: string, password: string) => {
-  return api.post("/login/", {
-    username,
-    password,
-  });
-};
--- mobile/src/services/dashboard.service.ts
+++
@@ -1,5 +0,0 @@
-import api from "./api.service";
-
-export const getDashboard = () => {
-  return api.get("/dashboard");
-};
